6372a96b86766a4b4ca30f9bbd986df76c6dd59f
Jack Malone <c00236428@itcarlow.ie>
2021-11-13 19:03:58 +0000

    Finished feature Add Inventory Component (Jira HP-1)

---
+++ Assets/Scripts/TestItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestItem : MonoBehaviour
+{
+    public GameObject spawner = null;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<TestInventoryPlayer>().UpdateText(spawner.GetComponent<TestItemSpawner>().item);
+            Debug.Log("Collision");
+            Destroy(this.gameObject);
+        }
+    }    
+}
