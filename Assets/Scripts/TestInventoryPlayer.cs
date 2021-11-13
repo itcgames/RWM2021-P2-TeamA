12ac9dc93147fe8da3f0d8d36fca4baa00bd79f3
Jack Malone <c00236428@itcarlow.ie>
2021-11-13 20:37:28 +0000

    Created item that the player is able to pick up

--- Assets/Scripts/TestInventoryPlayer.cs
+++ Assets/Scripts/TestInventoryPlayer.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestInventoryPlayer : MonoBehaviour
 {
     public Vector2 speed = new Vector2(20, 20);
+    public Text stackAmount;
+    public Text itemAmount;
+    private Inventory _inventory;
+    private int _itemCounter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _inventory = GetComponentInChildren<Inventory>();
+        _itemCounter = 0;
     }
 
     // Update is called once per frame
@@ -20,5 +26,19 @@ public class TestInventoryPlayer : MonoBehaviour
         Vector3 movement = new Vector3(speed.x * xInput, speed.y * yInput, 0);
         movement *= Time.deltaTime;
         transform.Translate(movement);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Item")
+        {
+            Debug.Log("colliding");
+            InventoryItem item = collision.gameObject.GetComponent<InventoryItem>();
+            _inventory.AddItem(item.gameObject, 1);
+            stackAmount.text = "Stack Amount: " + _inventory.Items.Count.ToString();
+            _itemCounter++;
+            itemAmount.text = "Total Items: " + _itemCounter.ToString();
+            Destroy(collision.gameObject);
+        }
     }
 }
