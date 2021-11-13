8679e0cb68de8c2dd1343849b5412630b7e1cff1
Jack Malone <c00236428@itcarlow.ie>
2021-11-13 20:18:44 +0000

    created basic player to be able to test item pick up

---
+++ Assets/Scripts/TestInventoryPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInventoryPlayer : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(20, 20);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(speed.x * xInput, speed.y * yInput, 0);
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
