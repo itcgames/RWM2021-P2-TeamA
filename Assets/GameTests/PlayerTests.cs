e64574bb91615146326b5a77b8e8c96b5e727ce2
Michael R Ryan <c00239510@itcarlow.ie>
2021-11-14 18:42:56 +0000

    [HP-10] Completed task 'Setup character prefab' with tests

---
+++ Assets/GameTests/PlayerTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class PlayerTests
+{
+    private TopdownCharacterController player;
+
+    [SetUp]
+    public void Setup()
+    {
+        SceneManager.LoadScene("Overworld", LoadSceneMode.Single);
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        if (player)
+            Object.Destroy(player.gameObject);
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerIsNotNull()
+    {
+        var playerObj = GameObject.Find("Player");
+        Assert.IsNotNull(playerObj);
+
+        player = playerObj.GetComponent<TopdownCharacterController>();
+        Assert.IsNotNull(player);
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerMovesCorrectly()
+    {
+        // Calls the previous test to get the player script and ensure it's
+        //      not null before running checks on it.
+        PlayerIsNotNull();
+
+        Assert.IsFalse(player.TilebasedMovement);
+        Assert.IsFalse(player.DiagonalMovementAllowed);
+        Assert.AreEqual(player.TimeToMaxSpeed, 0.0f);
+        Assert.AreEqual(player.TimeToFullStop, 0.0f);
+
+        yield return null;
+    }
+}
+
