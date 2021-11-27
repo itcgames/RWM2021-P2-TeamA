02468e1e17d297d8dc12bac5daae7dcba172ecd0
Michael R Ryan <c00239510@itcarlow.ie>
2021-11-27 15:34:13 +0000

    [HP-15] Completed task 'Add camera pan'

--- Assets/GameTests/CameraTests.cs
+++ Assets/GameTests/CameraTests.cs
@@ -6,9 +6,6 @@ using UnityEngine.TestTools;
 
 public class CameraTests
 {
-    private CameraFollowSnap _camSnap;
-    private Camera _camera;
-
     [SetUp]
     public void Setup()
     {
@@ -20,15 +17,69 @@ public class CameraTests
     {
         // Gets the camera components.
         var cameraObj = GameObject.Find("Main Camera");
-        _camSnap = cameraObj.GetComponent<CameraFollowSnap>();
-        _camera = cameraObj.GetComponent<Camera>();
+        CameraFollowSnap camSnap = cameraObj.GetComponent<CameraFollowSnap>();
+        Camera camera = cameraObj.GetComponent<Camera>();
 
         // Calculates half the camera size.
-        float height = _camera.orthographicSize;
-        Vector2 halfCameraSize = new Vector2(height * _camera.aspect, height);
+        float height = camera.orthographicSize;
+        Vector2 halfCameraSize = new Vector2(height * camera.aspect, height);
 
-        Assert.AreEqual(halfCameraSize, _camSnap.HalfAreaSize);
+        Assert.AreEqual(halfCameraSize, camSnap.HalfAreaSize);
 
         yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator CameraPansWhenTargetOutsideBounds()
+    {
+        // Gets the camera components and sets the initial values.
+        var cameraObj = GameObject.Find("Main Camera");
+        CameraFollowSnap camSnap = cameraObj.GetComponent<CameraFollowSnap>();
+        camSnap.secondsPerPan = 0.5f;
+        camSnap.boundaryOffset = new Vector2(0.5f, 0.5f);
+
+        // Gets the cam's current position and its destination.
+        Vector3 camsInitialPos = cameraObj.transform.position;
+        Vector3 camsDestination = camsInitialPos + Vector3.right *
+            camSnap.HalfAreaSize.x * 2.0f;
+
+        // Gets the player and moves it outside the camera bounds.
+        var playerObj = GameObject.Find("Player");
+        playerObj.transform.position = camsInitialPos +
+            Vector3.right * camSnap.HalfAreaSize.x;
+
+        // Checks that the camera begins moving but does not yet reach its
+        //      destination.
+        yield return new WaitForSeconds(0.25f);
+        Assert.AreNotEqual(camsInitialPos, cameraObj.transform.position);
+        Assert.AreNotEqual(camsDestination, cameraObj.transform.position);
+
+        // Check that the camera has reached its destination.
+        yield return new WaitForSeconds(0.3f);
+        Assert.AreEqual(camsDestination, cameraObj.transform.position);
+    }
+
+    [UnityTest]
+    public IEnumerator CameraPushesTargetWhilePanning()
+    {
+        // Gets the camera components and sets the initial values.
+        var cameraObj = GameObject.Find("Main Camera");
+        CameraFollowSnap camSnap = cameraObj.GetComponent<CameraFollowSnap>();
+        camSnap.secondsPerPan = 0.5f;
+        camSnap.boundaryOffset = new Vector2(0.5f, 0.5f);
+
+        // Gets the player and moves it outside the camera bounds.
+        var playerObj = GameObject.Find("Player");
+        playerObj.transform.position = cameraObj.transform.position +
+            Vector3.right * camSnap.HalfAreaSize.x;
+
+        // Finds the ideal player position and checks that it matches after
+        //      the pan time (plus a small margin for error).
+        Vector3 idealPlayerPos = playerObj.transform.position +
+            Vector3.right * camSnap.boundaryOffset.x;
+
+        yield return new WaitForSeconds(0.6f);
+
+        Assert.AreEqual(idealPlayerPos, playerObj.transform.position);
     }
 }
