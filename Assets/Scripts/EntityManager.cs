94c005b6bb1aac35bdfc894b1a1b3d5e9349ad91
Michael R Ryan <c00239510@itcarlow.ie>
2021-11-28 18:48:26 +0000

    [HP-25] Completed task 'Player freezes' with tests

--- Assets/Scripts/EntityManager.cs
+++ Assets/Scripts/EntityManager.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class EntityManager : MonoBehaviour
 {
     public CameraFollowSnap cameraMover;
+    public TopdownCharacterController playerController;
+    public Rigidbody2D playerRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         if (cameraMover)
+        {
             cameraMover.BeginMovementCallbacks.Add(OnCameraBeginMovement);
+            cameraMover.EndMovementCallbacks.Add(OnCameraMovementFinished);
+        }
     }
 
     private void OnCameraBeginMovement()
@@ -22,5 +27,20 @@ public class EntityManager : MonoBehaviour
         // Destroys all retrieved items and enemies.
         foreach (GameObject item in items) Destroy(item);
         foreach (GameObject enemy in enemies) Destroy(enemy);
+
+        // Disables player controller functionality.
+        if (playerController)
+            playerController.enabled = false;
+
+        // Clears any player velocity.
+        if (playerRigidbody)
+            playerRigidbody.velocity = Vector2.zero;
+    }
+
+    private void OnCameraMovementFinished()
+    {
+        // Enables player controller functionality.
+        if (playerController)
+            playerController.enabled = true;
     }
 }
