61de9564af4a3339e97fd5b3d3a645aff9555f08
Michael R Ryan <c00239510@itcarlow.ie>
2021-11-28 18:46:47 +0000

    [HP-25] Task completed. Added tests

--- Assets/GameTests/EntityManagerTests.cs
+++ Assets/GameTests/EntityManagerTests.cs
@@ -34,4 +34,35 @@ public class EntityManagerTests
         Assert.Zero(GameObject.FindGameObjectsWithTag("Item").Length);
         Assert.Zero(GameObject.FindGameObjectsWithTag("Enemy").Length);
     }
+
+    [UnityTest]
+    public IEnumerator PlayerFreezesOnCameraMovement()
+    {
+        // Gets the camera component and sets the initial values.
+        var cameraObj = GameObject.Find("Main Camera");
+        CameraFollowSnap camSnap = cameraObj.GetComponent<CameraFollowSnap>();
+        camSnap.secondsPerPan = 0.5f;
+
+        // Gets the player and moves it outside the camera bounds.
+        var playerObj = GameObject.Find("Player");
+        playerObj.transform.position = cameraObj.transform.position +
+            Vector3.right * camSnap.HalfAreaSize.x;
+
+        // Pauses for a fraction of a second to allow the camera to start moving.
+        yield return new WaitForSeconds(0.1f);
+
+        // Gets the player position for later comparison.
+        Vector3 previousPlayerPosition = playerObj.transform.position;
+
+        // Gets the player controller and tries to move right.
+        var playerController = playerObj.GetComponent<TopdownCharacterController>();
+        Assert.NotNull(playerController);
+        playerController.MoveRight(true);
+
+        // Pauses for a fraction of a second to allow the player to react.
+        yield return new WaitForSeconds(0.1f);
+
+        // Checks that the player has ignored the movement input.
+        Assert.AreEqual(previousPlayerPosition, playerObj.transform.position);
+    }
 }
