6bbdf91707c677a40b60451e9f698d211feba2a3
Michael R Ryan <c00239510@itcarlow.ie>
2021-12-04 22:41:34 +0000

    Added a rock projectile and octorok firing

--- Assets/Scripts/OctorokBehaviour.cs
+++ Assets/Scripts/OctorokBehaviour.cs
@@ -2,44 +2,70 @@
 
 public class OctorokBehaviour : EnemyBehaviour
 {
+    public GameObject projectilePrefab;
+    public float maxFireInterval = 10.0f;
+    public float projectileSpeed = 5.0f;
+
     private const float TIME_BETWEEN_ACTIONS = 1.0f;
     private float _lastMovementChange;
+    private float _lastShotFired;
 
     new void Start()
     {
         base.Start();
-        _lastMovementChange = Time.time;
+        _lastMovementChange = Time.time - TIME_BETWEEN_ACTIONS;
+        _lastShotFired = Time.time;
     }
 
     new void Update()
     {
         if (_lastMovementChange + TIME_BETWEEN_ACTIONS <= Time.time)
         {
+            // Random chance to fire a projectile.
+            ChanceToFireProjectile();
+
+            // Reset and pick a new action.
             _lastMovementChange = Time.time;
             Controller.ClearPersistentInput();
-
             int action = Random.Range(0, 5);
 
             // Moves in a direction depending on the action number. 0 == no movement.
             switch (action)
             {
-                case 1:
-                    Controller.MoveLeft(true);
-                    break;
-                case 2:
-                    Controller.MoveRight(true);
-                    break;
-                case 3:
-                    Controller.MoveDown(true);
-                    break;
-                case 4:
-                    Controller.MoveUp(true);
-                    break;
+                case 1: Controller.MoveLeft(true); break;
+                case 2: Controller.MoveRight(true); break;
+                case 3: Controller.MoveDown(true); break;
+                case 4: Controller.MoveUp(true); break;
             }
         }
 
         // Keeps to the area bounds.
         base.Update();
+    }
+
+    private void ChanceToFireProjectile()
+    {
+        float timeSinceLastShot = Time.time - _lastShotFired;
+        int shotChance = (int)Mathf.Max(maxFireInterval - timeSinceLastShot, 0.0f);
+
+        if (Random.Range(0, shotChance) == 0)
+        {
+            _lastShotFired = Time.time; // Resets the fired time.
+
+            Vector2 direction = new Vector2(Random.Range(-1, 2),
+                                            Random.Range(-1, 2));
+
+            // Handles cases of two 1s or two 0s.
+            if (direction.x != 0.0f && direction.y != 0.0f)
+                direction.x = 0.0f;
+            else if (direction.x == 0.0f && direction.y == 0.0f)
+                direction.x = 1.0f;
+
+            GameObject proj = Instantiate(projectilePrefab,
+                transform.position, Quaternion.identity);
+
+            proj.GetComponent<Projectile>()?.Fire(direction * projectileSpeed, AreaBounds);
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
