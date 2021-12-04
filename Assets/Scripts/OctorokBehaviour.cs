0dafe17138442d800fa5ff27ad33765dca942162
Michael R Ryan <c00239510@itcarlow.ie>
2021-12-04 15:55:45 +0000

    Added Octorok behaviour and movement

---
+++ Assets/Scripts/OctorokBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class OctorokBehaviour : CharacterBehaviour
+{
+    private const float TIME_BETWEEN_ACTIONS = 1.0f;
+    private float _lastMovementChange;
+
+    new void Start()
+    {
+        base.Start();
+        _lastMovementChange = Time.time;
+    }
+
+    void Update()
+    {
+        if (_lastMovementChange + TIME_BETWEEN_ACTIONS <= Time.time)
+        {
+            _lastMovementChange = Time.time;
+            Controller.ClearPersistentInput();
+
+            int action = Random.Range(0, 5);
+
+            // Moves in a direction depending on the action number. 0 == no movement.
+            switch (action)
+            {
+                case 1:
+                    Controller.MoveLeft(true);
+                    break;
+                case 2:
+                    Controller.MoveRight(true);
+                    break;
+                case 3:
+                    Controller.MoveDown(true);
+                    break;
+                case 4:
+                    Controller.MoveUp(true);
+                    break;
+            }
+        }
+    }
+}
