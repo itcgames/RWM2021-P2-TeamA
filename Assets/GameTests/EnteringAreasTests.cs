26c8f42afed4af3d58520750977c4b56d1a009b4
Michael R Ryan <c00239510@itcarlow.ie>
2021-12-06 18:20:11 +0000

    [HP-47] Completed task 'Area door detection' with a test

---
+++ Assets/GameTests/EnteringAreasTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+public class EnteringAreasTests
+{
+    // For the DoorDetectsThePlayer test.
+    bool doorEntered = false;
+    private void OnDoorEntered() => doorEntered = true;
+
+    [SetUp]
+    public void Setup()
+    {
+        SceneManager.LoadScene("Overworld", LoadSceneMode.Single);
+    }
+
+    [UnityTest]
+    public IEnumerator DoorDetectsThePlayer()
+    {
+        // Gets the player object.
+        var playerObj = GameObject.Find("Player");
+        Assert.IsNotNull(playerObj);
+
+        // Gets the door object and script.
+        var doorObj = GameObject.Find("CaveDoor");
+        Assert.IsNotNull(doorObj);
+        DoorDetector door = doorObj.GetComponent<DoorDetector>();
+        Assert.IsNotNull(door);
+
+        // Attaches the callback to the door.
+        Assert.IsFalse(doorEntered);
+        door.OnEnteredCallbacks.Add(OnDoorEntered);
+
+        // Move the player to the door and checks that the door has been entered.
+        playerObj.transform.position = doorObj.transform.position;
+        yield return new WaitForSeconds(0.1f);
+        Assert.IsTrue(doorEntered);
+    }
+}
---
+++ Assets/Scripts/DoorDetector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorDetector : MonoBehaviour
+{
+    public delegate void OnEntered();
+
+    public List<OnEntered> OnEnteredCallbacks { get; set; }
+
+    private void Awake()
+    {
+        OnEnteredCallbacks = new List<OnEntered>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (OnEnteredCallbacks.Count > 0)
+            {
+                foreach (var callback in OnEnteredCallbacks)
+                    callback();
+            }
+        }
+    }
+}
