061d56e262f35d7eb91ecd21eb0bba6303c89460
Kiernen Irons <C00241492@itcarlow.ie>
2021-12-06 11:15:47 +0000

    Full implementation of level editor

---
+++ Assets/Tilemap/Scripts/LoadingLevels.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class LoadingLevels : MonoBehaviour
+{
+    public TilemapVisual tilemapVisual;
+    private static int areas = 3; // Change this based on how many saves you have
+                                  // (if you only have 1 save you want to load then areas = 1, if you have 2 then areas = 2 etc.)
+    private Tilemap[] tilemap = new Tilemap[areas];
+    public Vector2[] locations = new Vector2 [areas];
+    private const int width = 16, height = 11;
+    private const float size = 0.9f;
+    string num = "";
+
+    float areaSizeX = (width * size);
+    float areaSizeY = (height * size);
+
+    void Start()
+    {
+        locations[0] = new Vector2(-areaSizeX / 2, -areaSizeY / 2);
+        locations[1] = new Vector2(areaSizeX / 2, -areaSizeY / 2);
+        locations[2] = new Vector2(-areaSizeX - (areaSizeX / 2), -areaSizeY / 2);
+
+        for (int i = 0; i < areas; i++)
+        {
+
+            if (i == 0) { }
+            else
+            {
+                num = "_" + (i + 1).ToString();
+            }
+
+            tilemap[i] = new Tilemap(width, height, size, locations[i]);
+            Debug.Log(num);
+
+            tilemap[i].SetTilemapVisual(Instantiate(tilemapVisual, tilemapVisual.transform.position, tilemapVisual.transform.rotation));
+            tilemap[i].Load("" + num);
+        }
+    }
+}
