5b028332517a7f237dc0e630503e26feef2475d4
Michael R Ryan <c00239510@itcarlow.ie>
2021-12-06 19:55:17 +0000

    [HP-48] Task complete. Added fade transitions and a test

--- Assets/Scripts/DoorDetector.cs
+++ Assets/Scripts/DoorDetector.cs
@@ -8,20 +8,51 @@ public class DoorDetector : MonoBehaviour
 
     public List<OnEntered> OnEnteredCallbacks { get; set; }
 
+    public CrossFade crossFade;
+    public EntityManager entityManager;
+
+    private bool _switching = false;
+
     private void Awake()
     {
         OnEnteredCallbacks = new List<OnEntered>();
     }
 
+    private void Start()
+    {
+        if (crossFade)
+        {
+            OnEnteredCallbacks.Add(crossFade.FadeToBlack);
+            crossFade.OnFadeCompleteCallbacks.Add(OnFadeEnd);
+        }
+
+        if (entityManager)
+            OnEnteredCallbacks.Add(entityManager.OnAreaChanged);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        // If not already switching and the collider is the player.
+        if (!_switching && collision.CompareTag("Player"))
         {
-            if (OnEnteredCallbacks.Count > 0)
-            {
-                foreach (var callback in OnEnteredCallbacks)
-                    callback();
-            }
+            // Start switching and call the callbacks.
+            _switching = true;
+            foreach (var callback in OnEnteredCallbacks)
+                callback();
         }
+    }
+
+    private void OnFadeEnd()
+    {
+        if (_switching)
+        {
+            _switching = false;
+
+            // TODO: Add scene switching functionality.
+
+            crossFade.FadeFromBlack();
+        }
+        else if (entityManager)
+            entityManager.UnpausePlayer();
     }
 }
