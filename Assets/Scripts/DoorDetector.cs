c08cf3f3f28daac0e6fc8c32950eb9faad90d2f1
Michael R Ryan <45245378+MichaelRRyan@users.noreply.github.com>
2021-12-06 22:33:31 +0000

    [HP-46] Completed feature 'Entering Areas' with tests
    * [HP-47] Completed task 'Area door detection' with a test
    
    * [HP-48] Completed task 'Area transition' with tests
    
    * [HP-49] Completed task 'Cave setup'
    
    * [HP-50] Completed task 'Visibility toggling' with tests
    
    * Removed new objects from Overworld
    
    * Readded content to Overworld
    
    * [HP-46] Feature complete. Merged master onto branch
    
    * Fixed issue with visibility toggling

---
+++ Assets/Scripts/DoorDetector.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorDetector : MonoBehaviour
+{
+    public delegate void OnEntered();
+
+    public List<OnEntered> OnEnteredCallbacks { get; set; }
+
+    // Whether or not this door is active on scene load.
+    public bool initiallyActive = false;
+
+    public CrossFade crossFade;
+    public EntityManager entityManager;
+    public Player player;
+    public Vector3 movePlayerTo = Vector3.zero;
+
+    public List<string> levelsFrom = new List<string>();
+    public List<string> levelsTo = new List<string>();
+
+    private List<GameObject> levelsToDisable = new List<GameObject>();
+    private List<GameObject> levelsToEnable = new List<GameObject>();
+    private bool _switching = false;
+
+    private void Awake()
+    {
+        OnEnteredCallbacks = new List<OnEntered>();
+    }
+
+    private void Start()
+    {
+        if (crossFade)
+        {
+            OnEnteredCallbacks.Add(crossFade.FadeToBlack);
+            crossFade.OnFadeCompleteCallbacks.Add(OnFadeEnd);
+        }
+
+        if (entityManager)
+            OnEnteredCallbacks.Add(entityManager.OnAreaChanged);
+
+        GetLevels();
+        StartCoroutine(DeactivateLevels());
+    }
+
+    private IEnumerator DeactivateLevels()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        // Disables all the levels to switch to.
+        if (initiallyActive)
+            foreach (GameObject level in levelsToEnable)
+                level.SetActive(false);
+    }
+
+    private void GetLevels()
+    {
+        // Gets a reference to all the levels to switch from.
+        foreach (string levelFrom in levelsFrom)
+        {
+            GameObject level = GameObject.Find(levelFrom);
+            if (level) levelsToDisable.Add(level);
+        }
+
+        // Gets a reference to all the levels to switch to.
+        foreach (string levelTo in levelsTo)
+        {
+            GameObject level = GameObject.Find(levelTo);
+            if (level) levelsToEnable.Add(level);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If not already switching and the collider is the player.
+        if (!_switching && collision.CompareTag("Player"))
+        {
+            // Start switching and call the callbacks.
+            _switching = true;
+            foreach (var callback in OnEnteredCallbacks)
+                callback();
+        }
+    }
+
+    private void OnFadeEnd()
+    {
+        if (_switching)
+        {
+            _switching = false;
+            SwitchLevelVisibilities();
+            crossFade.FadeFromBlack();
+        }
+        else if (entityManager)
+            entityManager.UnpausePlayer();
+    }
+
+    private void SwitchLevelVisibilities()
+    {
+        // Disables the levels from.
+        foreach (GameObject level in levelsToDisable)
+            level.SetActive(false);
+
+        // Enables the levels to.
+        foreach (GameObject level in levelsToEnable)
+            level.SetActive(true);
+
+        if (player) player.transform.position = movePlayerTo;
+    }
+}
--- Assets/Tilemap/Scripts/LoadingLevels.cs
+++ Assets/Tilemap/Scripts/LoadingLevels.cs
@@ -3,7 +3,7 @@
 public class LoadingLevels : MonoBehaviour
 {
     public TilemapVisual tilemapVisual;
-    private static int areas = 3; // Change this based on how many saves you have
+    private static int areas = 4; // Change this based on how many saves you have
                                   // (if you only have 1 save you want to load then areas = 1, if you have 2 then areas = 2 etc.)
     private Tilemap[] tilemap = new Tilemap[areas];
     private Vector3[] locations = new Vector3 [areas];
@@ -19,6 +19,7 @@ public class LoadingLevels : MonoBehaviour
         locations[0] = new Vector3(-areaSizeX / 2, -areaSizeY / 2, 2);
         locations[1] = new Vector3(areaSizeX / 2, -areaSizeY / 2, 2);
         locations[2] = new Vector3(-areaSizeX - (areaSizeX / 2), -areaSizeY / 2, 2);
+        locations[3] = new Vector3(-areaSizeX / 2, -areaSizeY / 2, 2);
 
         for (int i = 0; i < areas; i++)
         {
@@ -31,8 +32,10 @@ public class LoadingLevels : MonoBehaviour
 
             tilemap[i] = new Tilemap(width, height, size, locations[i]);
             //Debug.Log(num);
+            TilemapVisual level = Instantiate(tilemapVisual, tilemapVisual.transform.position, tilemapVisual.transform.rotation);
+            level.name = "Level " + i.ToString();
 
-            tilemap[i].SetTilemapVisual(Instantiate(tilemapVisual, tilemapVisual.transform.position, tilemapVisual.transform.rotation));
+            tilemap[i].SetTilemapVisual(level);
             tilemap[i].Load("" + num);
         }
     }
