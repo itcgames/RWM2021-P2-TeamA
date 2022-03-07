0896afc2db6c471f79e237d085e096bbc3a469ea
Evan McCormack <34337837+EvanMcCormack@users.noreply.github.com>
2022-03-07 12:06:23 +0000

    HP-56 Task Completed (#21)
    * HP-57_Camera Scrolls Needs minor fixes
    
    * HP-58_Can't seem to keep character on screen
    
    * HP-58_Character-Cant-Leave-Screen
    
    * HP-56 Tests fixed
    
    * HP-56 Tests Added

--- Assets/GameTests/CameraTests.cs
+++ Assets/GameTests/CameraTests.cs
@@ -30,56 +30,79 @@ public class CameraTests
     }
 
     [UnityTest]
-    public IEnumerator CameraPansWhenTargetOutsideBounds()
+    public IEnumerator PlayerStaysInFrame()
     {
-        // Gets the camera components and sets the initial values.
         var cameraObj = GameObject.Find("Main Camera");
-        CameraFollowSnap camSnap = cameraObj.GetComponent<CameraFollowSnap>();
-        camSnap.secondsPerPan = 0.5f;
-        camSnap.boundaryOffset = new Vector2(0.5f, 0.5f);
 
-        // Gets the cam's current position and its destination.
-        Vector3 camsInitialPos = cameraObj.transform.position;
-        Vector3 camsDestination = camsInitialPos + Vector3.right *
-            camSnap.HalfAreaSize.x * 2.0f;
+        Vector3 camInitPos = cameraObj.transform.position;
 
-        // Gets the player and moves it outside the camera bounds.
         var playerObj = GameObject.Find("Player");
-        playerObj.transform.position = camsInitialPos +
-            Vector3.right * camSnap.HalfAreaSize.x;
-
-        // Checks that the camera begins moving but does not yet reach its
-        //      destination.
+        var playerMoved = camInitPos + Vector3.left * cameraObj.GetComponent<CameraFollowSnap>().HalfAreaSize.x;
+        playerObj.transform.position = camInitPos + Vector3.left * cameraObj.GetComponent<CameraFollowSnap>().HalfAreaSize.x;
         yield return new WaitForSeconds(0.25f);
-        Assert.AreNotEqual(camsInitialPos, cameraObj.transform.position);
-        Assert.AreNotEqual(camsDestination, cameraObj.transform.position);
-
-        // Check that the camera has reached its destination.
-        yield return new WaitForSeconds(0.3f);
-        Assert.AreEqual(camsDestination, cameraObj.transform.position);
+        Assert.AreNotEqual(playerMoved, playerObj.transform.position);
     }
 
     [UnityTest]
-    public IEnumerator CameraPushesTargetWhilePanning()
+    public IEnumerator CameraMovesRight()
     {
-        // Gets the camera components and sets the initial values.
         var cameraObj = GameObject.Find("Main Camera");
-        CameraFollowSnap camSnap = cameraObj.GetComponent<CameraFollowSnap>();
-        camSnap.secondsPerPan = 0.5f;
-        camSnap.boundaryOffset = new Vector2(0.5f, 0.5f);
-
-        // Gets the player and moves it outside the camera bounds.
-        var playerObj = GameObject.Find("Player");
-        playerObj.transform.position = cameraObj.transform.position +
-            Vector3.right * camSnap.HalfAreaSize.x;
-
-        // Finds the ideal player position and checks that it matches after
-        //      the pan time (plus a small margin for error).
-        Vector3 idealPlayerPos = playerObj.transform.position +
-            Vector3.right * camSnap.boundaryOffset.x;
-
-        yield return new WaitForSeconds(0.6f);
-
-        Assert.AreEqual(idealPlayerPos, playerObj.transform.position);
+        Vector3 camInitPos = cameraObj.transform.position;
+        yield return new WaitForSeconds(0.5f);
+        Assert.AreNotEqual(camInitPos, cameraObj.transform.position);
     }
+
+    //[UnityTest]
+    //public IEnumerator CameraPansWhenTargetOutsideBounds()
+    //{
+    //    // Gets the camera components and sets the initial values.
+    //    var cameraObj = GameObject.Find("Main Camera");
+    //    CameraFollowSnap camSnap = cameraObj.GetComponent<CameraFollowSnap>();
+    //    camSnap.secondsPerPan = 0.5f;
+    //    camSnap.boundaryOffset = new Vector2(0.5f, 0.5f);
+
+    //    // Gets the cam's current position and its destination.
+    //    Vector3 camsInitialPos = cameraObj.transform.position;
+    //    Vector3 camsDestination = camsInitialPos + Vector3.right *
+    //        camSnap.HalfAreaSize.x * 2.0f;
+
+    //    // Gets the player and moves it outside the camera bounds.
+    //    var playerObj = GameObject.Find("Player");
+    //    playerObj.transform.position = camsInitialPos +
+    //        Vector3.right * camSnap.HalfAreaSize.x;
+
+    //    // Checks that the camera begins moving but does not yet reach its
+    //    //      destination.
+    //    yield return new WaitForSeconds(0.25f);
+    //    Assert.AreNotEqual(camsInitialPos, cameraObj.transform.position);
+    //    Assert.AreNotEqual(camsDestination, cameraObj.transform.position);
+
+    //    // Check that the camera has reached its destination.
+    //    yield return new WaitForSeconds(0.3f);
+    //    Assert.AreEqual(camsDestination, cameraObj.transform.position);
+    //}
+
+    //[UnityTest]
+    //public IEnumerator CameraPushesTargetWhilePanning()
+    //{
+    //    // Gets the camera components and sets the initial values.
+    //    var cameraObj = GameObject.Find("Main Camera");
+    //    CameraFollowSnap camSnap = cameraObj.GetComponent<CameraFollowSnap>();
+    //    camSnap.secondsPerPan = 0.5f;
+    //    camSnap.boundaryOffset = new Vector2(0.5f, 0.5f);
+
+    //    // Gets the player and moves it outside the camera bounds.
+    //    var playerObj = GameObject.Find("Player");
+    //    playerObj.transform.position = cameraObj.transform.position +
+    //        Vector3.right * camSnap.HalfAreaSize.x;
+
+    //    // Finds the ideal player position and checks that it matches after
+    //    //      the pan time (plus a small margin for error).
+    //    Vector3 idealPlayerPos = playerObj.transform.position +
+    //        Vector3.right * camSnap.boundaryOffset.x;
+
+    //    yield return new WaitForSeconds(0.6f);
+
+    //    Assert.AreEqual(idealPlayerPos, playerObj.transform.position);
+    //}
 }
---
+++ Assets/Scripts/Boundaries.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    public Camera mainCamera;
+    private Rect screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float cameraHeight = mainCamera.orthographicSize * 2;
+        float cameraWidth = cameraHeight * mainCamera.aspect;
+        Vector2 cameraSize = new Vector2(cameraWidth, cameraHeight);
+        Vector2 cameraCenterPosition = mainCamera.transform.position;
+        Vector2 cameraBottomLeftPos = cameraCenterPosition - (cameraSize / 2);
+        screenBounds = new Rect(cameraBottomLeftPos, cameraSize);
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        screenBounds.position = (Vector2)mainCamera.transform.position - (screenBounds.size / 2);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x + objectWidth, screenBounds.x + screenBounds.width - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y + objectHeight, screenBounds.y + screenBounds.height - objectHeight);
+        transform.position = viewPos;
+    }
+}
