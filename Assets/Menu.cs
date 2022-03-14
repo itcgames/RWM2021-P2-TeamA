577037c143dcad78bd6d4997c0aabce32b3e51cf
Evan McCormack <34337837+EvanMcCormack@users.noreply.github.com>
2022-03-14 09:33:21 +0000

    Hp 28 main menu (#27)
    * HP-69 Main Menu Completed
    
    * HP-70 Button Logic Complete
    
    * Updated Build Settings
    
    * Options Button Updated

---
+++ Assets/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+}
