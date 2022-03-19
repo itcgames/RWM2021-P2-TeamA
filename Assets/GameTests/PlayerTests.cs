a1c9aabc456e3d1916d5cb8eb099156e0aa384f8
Michael R Ryan <c00239510@itcarlow.ie>
2022-03-19 14:50:24 +0000

    [HP-74] Completed task 'No melee attack' with tests

--- Assets/GameTests/PlayerTests.cs
+++ Assets/GameTests/PlayerTests.cs
@@ -16,23 +16,15 @@ public class PlayerTests
     [UnityTest]
     public IEnumerator PlayerIsNotNull()
     {
-        var playerObj = GameObject.Find("Player");
-        Assert.IsNotNull(playerObj);
-
-        var player = playerObj.GetComponent<CharacterBehaviour>();
+        var player = GetPlayer();
         Assert.IsNotNull(player);
-
         yield return null;
     }
 
     [UnityTest]
     public IEnumerator PlayerMovesCorrectly()
     {
-        var playerObj = GameObject.Find("Player");
-        Assert.IsNotNull(playerObj);
-
-        var player = playerObj.GetComponent<CharacterBehaviour>();
-        Assert.IsNotNull(player);
+        var player = GetPlayer();
 
         Assert.Null(player.TilebasedMovement);
         Assert.IsTrue(player.TopdownMovement.DiagonalMovementAllowed);
@@ -40,6 +32,33 @@ public class PlayerTests
         Assert.AreEqual(player.TopdownMovement.TimeToFullStop, 2.0f);
 
         yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerHasNoMeleeAttack()
+    {
+        var player = GetPlayer();
+        Assert.Null(player.MeleeAttack);
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerHasRangedAttack()
+    {
+        var player = GetPlayer();
+        Assert.NotNull(player.RangedAttack);
+        yield return null;
+    }
+
+    private CharacterBehaviour GetPlayer()
+    {
+        var playerObj = GameObject.Find("Player");
+        Assert.IsNotNull(playerObj);
+
+        var player = playerObj.GetComponent<CharacterBehaviour>();
+        Assert.IsNotNull(player);
+
+        return player;
     }
 }
 
