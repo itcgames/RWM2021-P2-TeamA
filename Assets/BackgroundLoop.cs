91ad31023bd4282250eaa54736d1ba6f3ca60e0a
Evan McCormack <34337837+EvanMcCormack@users.noreply.github.com>
2022-03-21 14:27:37 +0000

    HP-71 Updated Background and looping

---
+++ Assets/BackgroundLoop.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnBecameInvisible()
+    {
+        transform.position = new Vector3(transform.position.x + (GetComponent<SpriteRenderer>().bounds.size.x * 2), transform.position.y, transform.position.z);
+    }
+}
