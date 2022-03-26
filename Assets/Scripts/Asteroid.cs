70ef75272ffe6ec3925d61648d8d6a05872d4856
Jack Malone <44910666+JackMalone1@users.noreply.github.com>
2022-03-26 17:36:46 +0000

    Completed Task Jira[HP-89] (#34)

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -9,6 +9,7 @@ public class Asteroid : CharacterBehaviour
     private Vector2 _velocity = new Vector2(-1, 0);
     private bool _persistentMovement = true;
     public string Type;
+    private Camera cam;
     public Vector2 Velocity { get; private set; }
     // Start is called before the first frame update
     new void Start()
@@ -16,12 +17,7 @@ public class Asteroid : CharacterBehaviour
         base.Start();
         Health.DeathCallbacks.Add(OnDeath);
         PickDirection();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        cam = Camera.main;
     }
 
     public void PickDirection()
@@ -40,11 +36,49 @@ public class Asteroid : CharacterBehaviour
         }
     }
 
+    public void SetDirection(Vector2 dir)
+    {
+        _velocity = dir;
+        _velocity.y = (Random.value < 0.5f) ? -1 : 1;
+        Movement.PreferHorizontal = true;
+        Movement.DiagonalMovement = true;
+        Movement.MoveLeft(_persistentMovement);
+        if (_velocity.y == -1)
+        {
+            Movement.MoveUp(_persistentMovement);
+        }
+        else
+        {
+            Movement.MoveDown(_persistentMovement);
+        }
+    }
+
     private void OnDeath(Dictionary<string, string> damageInfo)
     {
         if(nextAsteroid != null)
         {
             Instantiate(nextAsteroid, transform.position, Quaternion.identity);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        if(cam != null)
+        {
+            Vector2 viewportPosition = cam.WorldToViewportPoint(transform.position);
+
+            //if it goes off of the right hand screen we just want to destroy it we dont want it to screen wrap as well
+            if (viewportPosition.x < 0)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            //if it goes off of the top or the bottom of the screen then just wrap around to the other side of the screen.
+            if (viewportPosition.y > 1 || viewportPosition.y < 0)
+            {
+                transform.position = new Vector3(transform.position.x, -transform.position.y, 0);
+            }
         }
     }
 }
