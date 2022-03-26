b981e276dd030e405c39d86e11f2de49dfd82e31
Jack Malone <44910666+JackMalone1@users.noreply.github.com>
2022-03-26 16:17:47 +0000

    Completed task Jira[HP-87]
    * Setting up asteroid spawning
    
    * Working on movement
    
    * Fixed problem with moving left not being persistent
    
    * Updated spawn rates and fixed display of asteroids
    
    * Completed task Jira[HP-87]

---
+++ Assets/GameTests/AsteroidTests.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class AsteroidTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            SceneManager.LoadScene("Overworld", LoadSceneMode.Single);
+            List<GameObject> asteroids = GameObject.FindGameObjectsWithTag("Asteroid").ToList();
+        }
+
+        [UnityTest]
+        public IEnumerator SpawnerIsNotNull()
+        {
+            Spawner spawner = GetSpawner();
+            Assert.IsNotNull(spawner);
+            spawner.useCoRoutine = false;
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator SpawnerCreatesCorrectAsteroid()
+        {
+            Spawner spawner = GetSpawner();
+            spawner.useCoRoutine = false;
+            List<GameObject> asteroids;
+
+            spawner.Probability = 20;
+            spawner.CreateObject();
+
+            asteroids = GameObject.FindGameObjectsWithTag("Asteroid").ToList();
+
+            Assert.AreEqual(1, asteroids.Count);
+            Assert.AreEqual("Large", asteroids[0].GetComponent<Asteroid>().Type);
+
+            spawner.Probability = 40;
+            spawner.CreateObject();
+
+            asteroids = GameObject.FindGameObjectsWithTag("Asteroid").ToList();
+
+            Assert.AreEqual(2, asteroids.Count);
+            Assert.AreEqual("Medium", asteroids[1].GetComponent<Asteroid>().Type);
+
+            spawner.Probability = 80;
+            spawner.CreateObject();
+
+            asteroids = GameObject.FindGameObjectsWithTag("Asteroid").ToList();
+
+            Assert.AreEqual(3, asteroids.Count);
+            Assert.AreEqual("Small", asteroids[2].GetComponent<Asteroid>().Type);
+
+            yield return null;
+        }
+
+        private Spawner GetSpawner()
+        {
+            GameObject spawnerObj = GameObject.Find("AsteroidSpawner");
+            Assert.IsNotNull(spawnerObj);
+
+            Spawner spawner = spawnerObj.GetComponent<Spawner>();
+            Assert.IsNotNull(spawner);
+
+            return spawner;
+        }
+    }
+}
---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TopdownCharacterController;
+
+public class Asteroid : CharacterBehaviour
+{
+    public GameObject nextAsteroid;
+    private Vector2 _velocity = new Vector2(-1, 0);
+    private bool _persistentMovement = true;
+    public string Type;
+    public Vector2 Velocity { get; private set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        Health.DeathCallbacks.Add(OnDeath);
+        PickDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PickDirection()
+    {
+        _velocity.y = (Random.value < 0.5f) ? -1 : 1;
+        Movement.PreferHorizontal = true;
+        Movement.DiagonalMovement = true;
+        Movement.MoveLeft(_persistentMovement);
+        if(_velocity.y == -1)
+        {
+            Movement.MoveUp(_persistentMovement);
+        }
+        else
+        {
+            Movement.MoveDown(_persistentMovement);
+        }
+    }
+
+    private void OnDeath(Dictionary<string, string> damageInfo)
+    {
+        if(nextAsteroid != null)
+        {
+            Transform parentTransform = transform.parent;
+            GameObject obj = Instantiate(nextAsteroid, parentTransform.position, Quaternion.identity);
+            obj.transform.parent = parentTransform;
+        }
+    }
+}
