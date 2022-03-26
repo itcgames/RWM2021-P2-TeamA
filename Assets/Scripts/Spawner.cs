42dff1f42fe92dccdeb8c085513eda8a0b787ac0
Jack Malone <c00236428@itcarlow.ie>
2022-03-26 14:16:09 +0000

    Setting up asteroid spawning

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] objectsToSpawn;
+    public GameObject parent;
+    public int numberToSpawn;
+    public int limit = 20;
+    public float rate;
+    float spawnTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTimer = rate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(parent.transform.childCount < limit)
+        {
+            spawnTimer -= Time.deltaTime;
+            if(spawnTimer <= 0f)
+            {
+                for(int i = 0; i < numberToSpawn; ++i)
+                {
+                    GameObject objectToSpawn = objectsToSpawn[Random.Range(0, objectsToSpawn.Length)];
+                    GameObject obj = Instantiate(objectToSpawn, parent.transform.position, Quaternion.identity);
+                    obj.transform.parent = parent.transform;
+                }
+                spawnTimer = rate;
+            }
+        }
+    }
+
+    float GetModifier()
+    {
+        float modifier = Random.Range(0f, 1f);
+        if(Random.Range(0, 2) > 0)
+        {
+            return -modifier;
+        }
+        return modifier;
+    }
+}
